a8e4612762fa8ab1af4150104d1f834bf3a52006
Usevalad <usevaladbuben@gmail.com>
2024-11-20 16:49:11 +0300

    Setup login GUI
    .fxml file, background photo and controller

---
+++ client/src/main/java/org/course/client/controllers/LoginController.java
@@ -0,0 +1,43 @@
+package org.course.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class LoginController {
+    @FXML
+    private TextField PasswordField;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Text errorText;
+
+    @FXML
+    private Hyperlink forgetPasswordLink;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private Hyperlink registerLink;
+
+    @FXML
+    void onForgetPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRegister(ActionEvent event) {
+
+    }
+}
