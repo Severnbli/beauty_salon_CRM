6731543b74446b3b27fdac11f40b2539233c62a0
Usevalad <usevaladbuben@gmail.com>
2024-11-23 05:38:48 +0300

    Base logging for ServerMain and Main classes

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,19 +1,24 @@
+import org.apache.log4j.Logger;
 import server.ServerMain;
 
 import java.io.IOException;
 import java.util.logging.LogManager;
 
 public class Main {
+    public static final Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         loadProperties();
         loadServer();
     }
 
     public static void loadServer() {
+        log.info("Trying to load server...");
+
         try {
             ServerMain.startServer();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Failed to start server ", e);
         }
     }
 
