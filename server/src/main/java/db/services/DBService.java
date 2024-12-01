3ec455a12ce6f3bc92c90a0de118f9cdd86bcff7
Usevalad <usevaladbuben@gmail.com>
2024-12-01 21:40:17 +0300

    Create DBService.java
    Interface which performs work on components using a DAO

---
+++ server/src/main/java/db/services/DBService.java
@@ -0,0 +1,11 @@
+package db.services;
+
+import java.util.List;
+
+public interface DBService<T> {
+    T findById(Long id);
+    void save(T entity);
+    void delete(T entity);
+    void update(T entity);
+    List<T> getAll();
+}
