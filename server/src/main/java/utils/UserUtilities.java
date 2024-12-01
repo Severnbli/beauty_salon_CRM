d758685eaebdc9c2b8f0f9c8400260889506a160
Usevalad <usevaladbuben@gmail.com>
2024-12-01 22:54:44 +0300

    Create UserUtilities.java
    This class describe abilities for User class that used in ClientHandler

---
+++ server/src/main/java/utils/UserUtilities.java
@@ -0,0 +1,21 @@
+package utils;
+
+import db.services.UserService;
+import models.tcp.Request;
+import models.tcp.Response;
+
+public class UserUtilities {
+    private UserService userService = new UserService();
+
+    public void login(Request req, Response res) {
+
+    }
+
+    public void register(Request req, Response res) {
+
+    }
+
+    public void update(Request req, Response res) {
+
+    }
+}
