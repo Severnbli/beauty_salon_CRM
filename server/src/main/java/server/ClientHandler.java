19f94214155ff8ab55d8aa1b6c9676e6fd21fc23
Usevalad <usevaladbuben@gmail.com>
2024-12-01 22:53:31 +0300

    Complete ClientHandler.java with main user functionality

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -1,19 +1,99 @@
 package server;
 
-import lombok.*;
+import enums.ResponseStatus;
+import models.tcp.Request;
+import models.tcp.Response;
+import org.apache.log4j.Logger;
+import utils.UserUtilities;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-@NoArgsConstructor
-@RequiredArgsConstructor
-@Setter
-@Getter
 public class ClientHandler implements Runnable {
-    @NonNull
+    private static final Logger log = Logger.getLogger(ClientHandler.class);
     private Socket socket;
+    private Request request;
+    private Response response;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    UserUtilities userUtilities;
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new ObjectInputStream(socket.getInputStream());
+        out = new ObjectOutputStream(socket.getOutputStream());
+        userUtilities = new UserUtilities();
+    }
 
     @Override
     public void run() {
+        try {
+            while(socket.isConnected()) {
+                operate();
+            }
+        } catch (Exception e) {
+            log.error("Error while operating client: " + e);
 
+            response = new Response(ResponseStatus.ERROR, "Сервер закрыл соединение!", "");
+
+            try {
+                sendResponse();
+            } catch (IOException responseException) {
+                log.error("Error while writing goodbye response: " + responseException);
+            }
+        } finally {
+            close();
+        }
+    }
+
+    private void operate() throws IOException, ClassNotFoundException {
+        request = (Request) in.readObject();
+
+        switch (request.getType()) {
+            case LOGIN: {
+                userUtilities.login(request, response);
+                break;
+            }
+            case REGISTER: {
+                userUtilities.register(request, response);
+                break;
+            }
+            case UPDATE_PROFILE: {
+                userUtilities.update(request, response);
+                break;
+            }
+            default: {
+                response = new Response(ResponseStatus.ERROR, "Неизвестный запрос!", "");
+                break;
+            }
+        }
+
+        sendResponse();
+    }
+
+    private void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            log.error("Error while closing client streams: " + e);
+        } finally {
+            socket = null;
+            request = null;
+            response = null;
+            in = null;
+            out = null;
+            userUtilities = null;
+
+            System.gc();
+        }
+    }
+
+    private void sendResponse() throws IOException {
+        out.writeObject(response);
+        out.flush();
     }
 }
