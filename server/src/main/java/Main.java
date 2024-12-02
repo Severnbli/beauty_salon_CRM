39561f1e2fc4f21b92569a2a94e3b7436695092b
Usevalad <usevaladbuben@gmail.com>
2024-12-02 23:22:57 +0300

    Delete useless loading of properties

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,14 +3,12 @@ import server.ServerMain;
 import services.DBConnection;
 
 import java.io.IOException;
-import java.util.logging.LogManager;
 
 public class Main {
     public static final Logger log = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         try {
-            loadProperties();
             loadServer();
         } catch (Exception e) {
             log.error("Unhandled exception: ", e);
@@ -27,14 +25,6 @@ public class Main {
             ServerMain.startServer();
         } catch (IOException e) {
             log.error("Failed to start server ", e);
-        }
-    }
-
-    public static void loadProperties() {
-        try {
-            LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("/logging.properties"));
-        } catch (IOException e) {
-            System.err.println("Could not setup logger configuration: " + e);
         }
     }
 }
