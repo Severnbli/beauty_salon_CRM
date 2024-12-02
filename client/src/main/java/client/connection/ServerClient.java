0597b1bc9ebc3862f2629bbd68c436d1135faaf2
Usevalad <usevaladbuben@gmail.com>
2024-12-03 02:00:18 +0300

    Add logging to ServerClient class

--- client/src/main/java/client/connection/ServerClient.java
+++ client/src/main/java/client/connection/ServerClient.java
@@ -7,23 +7,29 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 @Getter
 public class ServerClient {
+    private static final Logger log = Logger.getLogger(ServerClient.class.getName());
+
     private static final ServerClient instance = new ServerClient();
-    private static Socket socket;
-    private static ObjectInputStream in;
-    private static ObjectOutputStream out;
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 
     ServerClient() {
         final Dotenv dotenv = Dotenv.load();
+
+        log.info("Trying to connect to server with credentials: IP("+ dotenv.get("SERVER_IP")
+                + ") & PORT("+ dotenv.get("SERVER_PORT")+ ")...");
 
         try {
             socket = new Socket(dotenv.get("SERVER_IP"), Integer.parseInt(dotenv.get("SERVER_PORT")));
             in = new ObjectInputStream(socket.getInputStream());
             out = new ObjectOutputStream(socket.getOutputStream());
         } catch (IOException e) {
-            e.printStackTrace();
+            log.severe("Attempt of connection to server failed. Reason: " + e);
         }
     }
 }
