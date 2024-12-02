f2647037b4c6f2529c05006f809b33dd346bc525
Usevalad <usevaladbuben@gmail.com>
2024-12-03 01:13:06 +0300

    Create Request.java

---
+++ client/src/main/java/client/tcp/Request.java
@@ -0,0 +1,13 @@
+package client.tcp;
+
+import client.enums.RequestType;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Request implements Serializable {
+    private RequestType type;
+    private String message;
+    private String data;
+}
