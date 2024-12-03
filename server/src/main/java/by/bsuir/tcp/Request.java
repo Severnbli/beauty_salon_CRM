7a09658ccc7dc318f016cedd8bad386395020a6a
Usevalad <usevaladbuben@gmail.com>
2024-12-03 19:08:38 +0300

    Update Request.java with constructors

--- server/src/main/java/by/bsuir/tcp/Request.java
+++ server/src/main/java/by/bsuir/tcp/Request.java
@@ -1,10 +1,14 @@
 package by.bsuir.tcp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Request implements Serializable {
     private RequestType type;
     private String message;
