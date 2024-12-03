2609b8297b50086a328ec12d21150f05bd70a237
Usevalad <usevaladbuben@gmail.com>
2024-12-03 15:35:14 +0300

    Add logging and server initialization

--- client/src/main/java/by/bsuir/client/App.java
+++ client/src/main/java/by/bsuir/client/App.java
@@ -1,11 +1,15 @@
 package by.bsuir.client;
 
+import by.bsuir.client.connection.ServerClient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.logging.Logger;
+
 public class App extends Application {
+    private static final Logger log = Logger.getLogger(App.class.getName());
     private static final String TITLE_NAME = "CRM - Салон красоты";
 
     @Override
@@ -14,5 +18,7 @@ public class App extends Application {
         stage.setTitle(TITLE_NAME);
         stage.setResizable(false);
         stage.show();
+
+        log.info("Server info: " + ServerClient.getInstance()); // Server initialization
     }
 }
