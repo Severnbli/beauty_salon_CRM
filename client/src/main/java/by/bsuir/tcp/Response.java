e03e5bf4406dd7acea17d0474800a0fe6cec6f82
Usevalad <usevaladbuben@gmail.com>
2024-12-03 15:38:02 +0300

    Add constructors to Response class

--- client/src/main/java/by/bsuir/tcp/Response.java
+++ client/src/main/java/by/bsuir/tcp/Response.java
@@ -1,9 +1,13 @@
 package by.bsuir.tcp;
 
 import by.bsuir.enums.ResponseStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Response {
     private ResponseStatus status;
     private String message;
