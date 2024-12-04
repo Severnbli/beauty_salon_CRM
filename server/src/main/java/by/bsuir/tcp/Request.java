7a6d4a351bf56e11910dfa6ba46bf07a455a6484
Usevalad <usevaladbuben@gmail.com>
2024-12-04 03:37:09 +0300

    Add builder for tcp classes

--- server/src/main/java/by/bsuir/tcp/Request.java
+++ server/src/main/java/by/bsuir/tcp/Request.java
@@ -1,14 +1,12 @@
 package by.bsuir.tcp;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class Request implements Serializable {
     private RequestType type;
     private String message;
